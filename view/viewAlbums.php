d60ca9f5553786c4058fbe753e5c2be0ee500210
Johan Lund <johanjohan.lund@gmail.com>
2017-04-10 14:41:32 +0200

    added view for displaying album information

---
+++ view/viewAlbums.php
@@ -0,0 +1,15 @@
+<html>
+<head></head>
+
+<body>
+
+<?php
+
+echo 'Title:' . $album->title . '<br/>';
+echo 'Author:' . $album->artist . '<br/>';
+echo 'Description:' . $album->year . '<br/>';
+
+?>
+
+</body>
+</html>
