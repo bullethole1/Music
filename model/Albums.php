d68140de319b407064be427167dcf628000651e3
Johan Lund <johanjohan.lund@gmail.com>
2017-04-10 14:26:29 +0200

    added class for holding album information an removed template folder

---
+++ model/Albums.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: johanlund
+ * Date: 2017-04-10
+ * Time: 14:12
+ */
+/*Object to hold album information*/
+
+class Albums
+{
+    private $id;
+    private $title;
+    private $artist;
+    private $year;
+
+    function __construct($id, $title, $artist, $year)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->artist = $artist;
+        $this->year = $year;
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * @param mixed $artist
+     */
+    public function setArtist($artist)
+    {
+        $this->artist = $artist;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param mixed $year
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
+
+}
