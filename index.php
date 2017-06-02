cd65b9ae5ddc99e3d8fd7d08100d5d844bb4a4c2
Johan Lund <johanjohan.lund@gmail.com>
2017-06-02 08:29:38 +0200

    removed redundant database class

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-require('resources/Database.php');
-require('resources/Controller.php');
+require('Controller/Controller.php');
 require('model/Model.php');
 require('model/Album.php');
 
 $config = require('resources/config.php');
-$db = new Database($config);
+$pdo = new PDO($config['db_type'] . ':host=' . $config['db_host'] . ';dbname=' . $config['db_name'], $config['db_username'], $config['db_password']);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$controller = new Controller($db);
+$controller = new Controller($pdo);
 $controller->index();
